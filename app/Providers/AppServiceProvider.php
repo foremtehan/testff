1c0c6e7e563c71560065162daf5b6d552acf3f6d
wthreex <untymage@gmail.com>
2020-06-08 01:17:01 +0430

    Ok

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Validator::extend('spamfree', 'App\Rules\SpamFree@passes');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
